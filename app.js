b29991bd13d0dfde13d2c349571c8f3bcb71ba43
mykaya24 <78302635+mykaya24@users.noreply.github.com>
2026-01-31 23:02:44 +0300

    template engine

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const app = express();
 
-const port = 3000;
+const app = express();
+app.set('view engine',"ejs");
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.send('Index page');
+  res.render('index',{
+    page_name:"index"
+  });
 });
 
+app.get('/about', (req, res) => {
+    res.render('about',{
+        page_name:"about"
+    });
+  });
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App started on ${port}`);
 });
